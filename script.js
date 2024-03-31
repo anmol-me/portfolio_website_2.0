e11f233942c0d57fc8cf18a44e7ace2fd9fa8192
Anmol <39460326+anmol-me@users.noreply.github.com>
2024-03-31 16:21:12 +0530

    Persist theme settings using localStorage

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
 const iconButton = document.querySelector(".icon");
+const body = document.body;
+let isDarkMode = localStorage.getItem("isDarkMode") === "true";
 
 iconButton.addEventListener("click", toggleMode);
 
 function toggleMode() {
-  // Toggles theme mode
-  document.body.classList.toggle("dark-mode");
+  // Add & remove transition effect to background
+  // Avoids theme animation on page refresh
+  body.style.transition = "background-color 0.5s ease";
+  setTimeout(function () {
+    body.style.transition = "";
+  }, 600);
 
-  // Toggles icon
-  if (document.body.classList.contains("dark-mode")) {
+  // Toggle theme mode
+  body.classList.toggle("dark-mode");
+  isDarkMode = !isDarkMode;
+
+  // Toggle icon
+  if (isDarkMode) {
     iconButton.src = "assets/icons/light_mode.svg";
   } else {
     iconButton.src = "assets/icons/dark_mode.svg";
   }
+
+  localStorage.setItem("isDarkMode", isDarkMode);
+}
+
+// Load theme settings on page refresh
+if (isDarkMode) {
+  body.classList.add("dark-mode");
+  iconButton.src = "assets/icons/light_mode.svg";
 }
 
 function updateCopyrightYear() {
